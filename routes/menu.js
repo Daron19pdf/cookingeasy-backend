60610b5c314df476af51cff4513c4838de837504
vpalma78-01@yahoo.fr <117671625+RonickP@users.noreply.github.com>
2023-03-02 14:29:36 +0100

    df

--- routes/menu.js
+++ routes/menu.js
@@ -6,6 +6,31 @@ const Preference = require("../models/preference");
 const uid2 = require("uid2");
 const fetch = require('node-fetch')
 
+router.get("/preference", (req, res) => {
+    const { vegetarien,vegan,pescetarien,gluten,porc,alcool,lactose,sansRegimeParticulier, token } = req.body;
+    User.findOne({ token: token }).then(async (user) => {
+      if (user) {
+        const preferenceUser = await Preference.findById(user.preference);
+          preferenceUser.regime = {
+            vegetarien: vegetarien,
+            vegan: vegan,
+            pescetarien: pescetarien,
+            gluten: gluten,
+            porc: porc, 
+            alcool: alcool,
+            lactose: lactose, 
+            sansRegimeParticulier : sansRegimeParticulier,
+            token: token,
+          };
+          preferenceUser.save().then(() => {
+            res.json({ result: true });
+          });
+      } else {
+        res.json({ result: false, error: "Utilisateur inexistant." });
+      }
+    });
+
+  });
 
 
 
