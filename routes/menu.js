95e7b6a365d8921bb62860d69442e8343d2e82b7
vpalma78-01@yahoo.fr <117671625+RonickP@users.noreply.github.com>
2023-03-02 14:12:42 +0100

    f

---
+++ routes/menu.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var router = express.Router();
+const mongoose = require("mongoose");
+const User = require("../models/users");
+const Preference = require("../models/preference");
+const uid2 = require("uid2");
+const fetch = require('node-fetch')
+
+
+
+
+
+
+
+  
+  
+
+
+  module.exports = router;
