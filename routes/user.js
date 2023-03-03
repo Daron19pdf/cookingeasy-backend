fd509a0f36d3117d8df5a5f661d8476f127a1a8e
vpalma78-01@yahoo.fr <117671625+RonickP@users.noreply.github.com>
2023-03-03 15:38:26 +0100

    fdv

--- routes/user.js
+++ routes/user.js
@@ -89,5 +89,19 @@ router.delete('/:token', async (req, res) => {
   }
 });
 
+router.get("/:token", (req, res) => {
+  User.findOne({ token: req.query.token })
+    .populate("preference")
+    .then((data) => {
+      if (data) {
+        res.json({
+          result: true,
+          data
+        });
+      } else {
+        res.json({ result: false, error: "Utilisateur inexistant" });
+      }
+    });
+});
 
 module.exports = router;
